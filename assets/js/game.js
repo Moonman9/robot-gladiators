8f4b5bdbd9195ffdbd370397813f4697a3963bce
Moonman9 <zzepold97@gmail.com>
2022-02-07 17:25:00 -0700

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,48 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10+10);
-console.log("Our robot's name is " + playerName);
+console.log(playerName, playerAttack, playerHealth);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+console.log(enemyName, enemyAttack, enemyHealth);
 
 function fight() {
-    window.alert("The fight has begun!");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
+    if (promptFight === "figth" || promptFight === "FIGHT") {
+        window.alert("The fight has begun!");
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
+            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        );
+        if (enemyHealth <= 0) {
+            window.alert(enemyName + " has died!");
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
+        playerHealth = (playerHealth - enemyAttack);
+        console.log(
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        );
+        //check players health
+        if (playerHealth <= 0) {
+            window.alert(playerName + " has died!")
+        }
+        else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            playerMoney = playerMoney - 2;
+            else {
+                fight();
+            }
+        }
+    }
 }
-//fight();
+fight();
